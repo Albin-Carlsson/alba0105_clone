e304678b819a264537c272002c11f37e53a1b4e1
alexabarbu <alexabarbu37@gmail.com>
2024-09-27 14:00:07 +0200

    assignment1_step11

---
+++ inluppar/inlupp1/iterator.c
@@ -0,0 +1,100 @@
+#include "iterator.h"
+#include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+#include <string.h>
+
+
+typedef struct iter ioopm_list_iterator_t;
+
+
+ioopm_list_iterator_t *ioopm_list_iterator(ioopm_list_t *list) {
+    ioopm_list_iterator_t *new_iter = malloc(sizeof(ioopm_list_iterator_t));
+    if (!new_iter) return NULL;
+    new_iter->current = list->first;
+    new_iter->list = list;
+    return new_iter;
+}
+
+
+bool ioopm_iterator_has_next(ioopm_list_iterator_t *iter) {
+    if (iter->current == NULL) return false;
+    return iter->current->next != NULL;
+}
+
+
+
+int ioopm_iterator_next(ioopm_list_iterator_t *iter) {
+    if (iter->current == NULL) {
+        puts("Iterator is at the end of the list or the list is empty");
+        return 0; // Eller hantera det på ett sätt som passar din applikation
+    }
+    ioopm_link_t * new_link = iter->current->next;
+    if (new_link == NULL) {
+        puts("End of list");
+        return 0;
+    }
+    iter->current = new_link;
+    return new_link->value;
+}
+
+
+void ioopm_iterator_reset(ioopm_list_iterator_t *iter){
+
+    iter->current = iter->list->first;
+}
+
+
+int ioopm_iterator_current(ioopm_list_iterator_t *iter){
+    int elem = iter->current->value;
+
+    return elem;
+}
+
+void ioopm_iterator_destroy(ioopm_list_iterator_t *iter){
+    free(iter);
+}
+
+
+
+static ioopm_link_t * find_previous_link(ioopm_list_iterator_t *iter, ioopm_list_t * list){
+
+    ioopm_link_t * current = list->first->next;
+
+    while(current){
+
+        if(current == iter->current){
+            return current;
+        }
+        current = current->next;
+    }
+
+    return iter->current;
+}
+
+/*
+int ioopm_iterator_remove(ioopm_list_iterator_t *iter){
+
+    ioopm_link_t * new_current= iter->current->next;
+
+    ioopm_link_t * to_remove = iter->current;
+
+    int to_remove_value = to_remove->value;
+
+    free(to_remove);
+
+    ioopm_list_t * list= iter->list; 
+
+    ioopm_link_t * prev = find_previous_link(iter, list);
+
+    prev->next = new_current;
+
+    iter->current = new_current;
+
+    return to_remove_value;
+
+}
+*/
---
+++ inluppar/inlupp1/iterator_tests.c
@@ -0,0 +1,187 @@
+#include "iterator.h"
+#include <CUnit/Basic.h>
+
+
+int init_suite(void) {
+  // Change this function if you want to do something *before* you
+  // run a test suite
+  return 0;
+}
+
+int clean_suite(void) {
+  // Change this function if you want to do something *after* you
+  // run a test suite
+  return 0;
+}
+
+
+void test_iter_has_next(){
+
+    ioopm_list_t * list = ioopm_linked_list_create();
+
+    ioopm_linked_list_append(list, 1);
+
+    ioopm_list_iterator_t * iter = ioopm_list_iterator(list);
+
+    bool result = ioopm_iterator_has_next(iter);
+
+    CU_ASSERT_TRUE(result);
+
+    ioopm_iterator_destroy(iter);
+    ioopm_linked_list_destroy(list);
+
+}
+
+void test_iter_next(){
+
+ioopm_list_t * list = ioopm_linked_list_create();
+ioopm_list_iterator_t * iter = ioopm_list_iterator(list);
+
+ ioopm_linked_list_append(list, 1);
+ ioopm_linked_list_append(list, 2);
+
+ int elem = ioopm_iterator_next(iter);
+
+ CU_ASSERT_EQUAL(1, elem);
+
+  elem = ioopm_iterator_next(iter);
+
+ CU_ASSERT_EQUAL(2, elem);
+
+ ioopm_iterator_destroy(iter);
+
+ ioopm_linked_list_destroy(list);
+
+}
+
+void test_iter_reset(){
+ioopm_list_t * list = ioopm_linked_list_create();
+ioopm_list_iterator_t * iter = ioopm_list_iterator(list);
+
+ ioopm_linked_list_append(list, 1);
+ ioopm_linked_list_append(list, 2);
+
+ int elem = ioopm_iterator_next(iter);
+ elem = ioopm_iterator_next(iter);
+
+  CU_ASSERT_EQUAL(2, elem);
+
+
+ ioopm_iterator_reset(iter);
+ elem = ioopm_iterator_next(iter);
+
+ CU_ASSERT_EQUAL(1, elem);
+
+ ioopm_iterator_destroy(iter);
+ ioopm_linked_list_destroy(list);
+
+}
+
+
+void test_iter_current(){
+
+ioopm_list_t * list = ioopm_linked_list_create();
+ioopm_list_iterator_t * iter = ioopm_list_iterator(list);
+
+ ioopm_linked_list_append(list, 1);
+ ioopm_linked_list_append(list, 2);
+
+ int elem = ioopm_iterator_next(iter);
+ elem = ioopm_iterator_next(iter);
+
+ int val = ioopm_iterator_current(iter);
+
+ CU_ASSERT_EQUAL(2, val);
+
+  ioopm_iterator_destroy(iter);
+ ioopm_linked_list_destroy(list);
+
+
+
+}
+
+/*
+void test_iter_remove(){
+
+ioopm_list_t * list = ioopm_linked_list_create();
+ioopm_list_iterator_t * iter = ioopm_list_iterator(list);
+
+ ioopm_linked_list_append(list, 1);
+ ioopm_linked_list_append(list, 2);
+ ioopm_linked_list_append(list, 3);
+
+  int elem = ioopm_iterator_next(iter);
+ elem = ioopm_iterator_next(iter);
+
+ ioopm_iterator_remove(iter);
+
+ elem = ioopm_iterator_current(iter);
+
+
+ CU_ASSERT_EQUAL(3, elem);
+
+   ioopm_iterator_destroy(iter);
+ ioopm_linked_list_destroy(list);
+ 
+}
+*/
+
+
+
+
+
+
+
+
+int main() {
+  // First we try to set up CUnit, and exit if we fail
+  if (CU_initialize_registry() != CUE_SUCCESS)
+    return CU_get_error();
+
+  // We then create an empty test suite and specify the name and
+  // the init and cleanup functions
+  CU_pSuite my_test_suite = CU_add_suite("My awesome test suite", init_suite, clean_suite);
+  if (my_test_suite == NULL) {
+      // If the test suite could not be added, tear down CUnit and exit
+      CU_cleanup_registry();
+      return CU_get_error();
+  }
+
+  // This is where we add the test functions to our test suite.
+  // For each call to CU_add_test we specify the test suite, the
+  // name or description of the test, and the function that runs
+  // the test in question. If you want to add another test, just
+  // copy a line below and change the information
+  if (
+    (CU_add_test(my_test_suite, "test iter has next", test_iter_has_next) == NULL) ||
+    (CU_add_test(my_test_suite, "test iter next", test_iter_next) == NULL) ||
+    (CU_add_test(my_test_suite, "test iter reset", test_iter_reset) == NULL) ||
+    (CU_add_test(my_test_suite, "test iter current", test_iter_current) == NULL) ||
+    //(CU_add_test(my_test_suite, "test iter remove", test_iter_remove) == NULL) ||
+    
+    
+
+    
+    
+
+
+    0
+  )
+    {
+      // If adding any of the tests fails, we tear down CUnit and exit
+      CU_cleanup_registry();
+      return CU_get_error();
+    }
+
+  // Set the running mode. Use CU_BRM_VERBOSE for maximum output.
+  // Use CU_BRM_NORMAL to only print errors and a summary
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+  // This is where the tests are actually run!
+  CU_basic_run_tests();
+
+  // Tear down CUnit before exiting
+  CU_cleanup_registry();
+  return CU_get_error();
+
+}
